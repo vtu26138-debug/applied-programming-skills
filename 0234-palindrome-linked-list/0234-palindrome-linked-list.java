7bf47931eccb7337c179191770610eaf61b3193d
vtu26138-debug <vtu26138@veltech.edu.in>
2026-02-01 14:32:28 +0530

    Time: 7 ms (34.19%), Space: 99.7 MB (22.74%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        List<Integer> arr = new ArrayList<>();
+
+        while (head != null) {
+            arr.add(head.val);
+            head = head.next;
+        }
+
+        int left = 0;
+        int right = arr.size() - 1;
+
+        while (left < right) {
+            if (!arr.get(left).equals(arr.get(right))) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;        
+    }
+}
