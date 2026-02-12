6c44e70132d9ca12a69a5206998d45e46734c675
vtu26138-debug <vtu26138@veltech.edu.in>
2026-02-12 16:36:39 +0530

    Time: 1 ms (99.69%), Space: 44.1 MB (76.14%) - LeetHub

---
+++ 0257-binary-tree-paths/0257-binary-tree-paths.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> result = new ArrayList<>();
+        if (root == null) return result;
+
+        TreePaths(root, new StringBuilder(), result);
+        return result;
+    }
+
+    void TreePaths(TreeNode root, StringBuilder path, List<String> result) {
+        int len = path.length();   // save state for backtracking
+
+        path.append(root.val);
+
+        if (root.left == null && root.right == null) {
+            result.add(path.toString());
+        } else {
+            path.append("->");
+            if (root.left != null) TreePaths(root.left, path, result);
+            if (root.right != null) TreePaths(root.right, path, result);
+        }
+
+        path.setLength(len); // backtrack (removes val and "->")
+    }
+}
